ba1b56d2a5075df3818fb69450921ff2df1fddeb
aurz1599 <aurz1599@gmail.com>
2022-08-06 16:38:07 +0700

    Update Home Pages
    update Ui on Home Pages but not finish

--- reminedevice/src/components/Timer.jsx
+++ reminedevice/src/components/Timer.jsx
@@ -1,11 +1,15 @@
 import React, { Component } from 'react'
+import styled from "styled-components";
 
 const Timer = () => {
     let time = new Date().toLocaleTimeString();
     //let curTime = time.getHours() + ':' + time.getMinutes() + ':' + time.getSeconds();
-     
+    const Time = {
+        fontSize: "64px",
+        color: "#FF9393"
+    }
     return(
-        <h1>{time}</h1>
+        <h1 style={Time}>{time}</h1>
     );
 }
 export default Timer;
--- reminedevice/src/pages/Home.js
+++ reminedevice/src/pages/Home.js
@@ -1,13 +1,44 @@
 import React from 'react'
 import { render } from 'react-dom';
 import Timer from '../components/Timer';
+import pillIcon from '../images/pill_icon.png'
+import settingIcon from '../images/setting_icon.png'
+import setting from '../images/setting.png'
+import add from '../images/add.png'
+import styled from "styled-components";
+
+const Button = styled.button`
+  background-color: white;
+  color: black;
+  font-size: 36px;
+  padding: 10px 60px;
+  border-radius: 50px;
+  margin: 10px 0px;
+`;
+
+const ImageIcon = styled.image`
+  padding: 20px 20px;
+`;
 
 export default function Home() {
   return (
-    <div>
+    <div className='App'>
       <Timer/>
-      <button>ยาใหม่</button>
-      <button>ตั้งค่า</button>
+      <Button>
+        <img src={pillIcon} height={90} width={90} alt="Add New Pill Icon"/>
+        <p>
+          <img src={add}/>
+          ยาใหม
+        </p>
+      </Button>
+      
+      <Button>
+        <img src={settingIcon} height={90} width={90} alt="Setting Icon"/>
+        <p>
+          <img src={setting}/>
+          ตั้งค่า
+        </p>
+      </Button>
     </div>
   )
 }
